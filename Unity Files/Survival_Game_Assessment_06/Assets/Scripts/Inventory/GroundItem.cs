8cc5846ec7082acbcc8c43b613b084588d60f8a4
RaptorTech4 <bennet.rumboll@gmail.com>
2021-09-30 17:37:46 +0200

    UPDATE

--- Unity Files/Survival_Game_Assessment_06/Assets/Scripts/Inventory/GroundItem.cs
+++ Unity Files/Survival_Game_Assessment_06/Assets/Scripts/Inventory/GroundItem.cs
@@ -14,8 +14,8 @@ public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
     public void OnBeforeSerialize()
     {
 #if UNITY_EDITOR
-        GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
-        EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
+        //GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
+        //EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
 #endif
     }
 }
